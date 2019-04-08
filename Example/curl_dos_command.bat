5f4b17afe9b94f63706752457f5ac3bc06ac2720
LILINOpenGitHub <38514730+LILINOpenGitHub@users.noreply.github.com>
2019-04-08 20:39:08 +0800

    Update curl_dos_command.bat

--- Example/curl_dos_command.bat
+++ Example/curl_dos_command.bat
@@ -1,4 +1,4 @@
 :LOOP
-curl --request POST --data-binary "@D:\your_folder\pic_of_plate.jpg" http://127.0.0.1:8592/sendjpeg
+curl --request POST --data-binary "@D:\your_folder\sample.jpg" http://127.0.0.1:8592/sendjpeg
 @ping 127.0.0.1 -n 1 > NUL
 @goto :LOOP
