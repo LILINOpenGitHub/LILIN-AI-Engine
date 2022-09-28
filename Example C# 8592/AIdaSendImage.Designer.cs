893a1bb65d07379d8f6f9b0fb510b9208d8c60db
Merit LILIN Enterprise Co., LTD <38514730+LILINOpenGitHub@users.noreply.github.com>
2022-09-28 13:09:39 +0800

    Add files via upload

---
+++ Example C# 8592/AIdaSendImage.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace LILIN_Aida_Test
+{
+    partial class AIdaSendImage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Tb_Password = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Tb_Account = new System.Windows.Forms.TextBox();
+            this.Tb_IP = new System.Windows.Forms.TextBox();
+            this.Tb_Port = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.Btn_Send = new System.Windows.Forms.Button();
+            this.Btn_Browse = new System.Windows.Forms.Button();
+            this.pb_image = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_image)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Tb_Password
+            // 
+            this.Tb_Password.Location = new System.Drawing.Point(771, 24);
+            this.Tb_Password.Margin = new System.Windows.Forms.Padding(5);
+            this.Tb_Password.Name = "Tb_Password";
+            this.Tb_Password.Size = new System.Drawing.Size(125, 27);
+            this.Tb_Password.TabIndex = 45;
+            this.Tb_Password.Text = "pass1234";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 27);
+            this.label1.Margin = new System.Windows.Forms.Padding(5);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 16);
+            this.label1.TabIndex = 40;
+            this.label1.Text = "IP:";
+            // 
+            // Tb_Account
+            // 
+            this.Tb_Account.Location = new System.Drawing.Point(510, 24);
+            this.Tb_Account.Margin = new System.Windows.Forms.Padding(5);
+            this.Tb_Account.Name = "Tb_Account";
+            this.Tb_Account.Size = new System.Drawing.Size(125, 27);
+            this.Tb_Account.TabIndex = 46;
+            this.Tb_Account.Text = "admin";
+            // 
+            // Tb_IP
+            // 
+            this.Tb_IP.Location = new System.Drawing.Point(49, 24);
+            this.Tb_IP.Margin = new System.Windows.Forms.Padding(5);
+            this.Tb_IP.Name = "Tb_IP";
+            this.Tb_IP.Size = new System.Drawing.Size(145, 27);
+            this.Tb_IP.TabIndex = 47;
+            this.Tb_IP.Text = "127.0.0.1";
+            // 
+            // Tb_Port
+            // 
+            this.Tb_Port.Location = new System.Drawing.Point(290, 24);
+            this.Tb_Port.Margin = new System.Windows.Forms.Padding(5);
+            this.Tb_Port.Name = "Tb_Port";
+            this.Tb_Port.Size = new System.Drawing.Size(81, 27);
+            this.Tb_Port.TabIndex = 48;
+            this.Tb_Port.Text = "8592";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(435, 27);
+            this.label4.Margin = new System.Windows.Forms.Padding(5);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 16);
+            this.label4.TabIndex = 41;
+            this.label4.Text = "Account:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(243, 27);
+            this.label2.Margin = new System.Windows.Forms.Padding(5);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 16);
+            this.label2.TabIndex = 42;
+            this.label2.Text = "Port:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(690, 27);
+            this.label5.Margin = new System.Windows.Forms.Padding(5);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(71, 16);
+            this.label5.TabIndex = 44;
+            this.label5.Text = "Password:";
+            // 
+            // Btn_Send
+            // 
+            this.Btn_Send.Location = new System.Drawing.Point(112, 87);
+            this.Btn_Send.Margin = new System.Windows.Forms.Padding(15);
+            this.Btn_Send.Name = "Btn_Send";
+            this.Btn_Send.Size = new System.Drawing.Size(65, 29);
+            this.Btn_Send.TabIndex = 50;
+            this.Btn_Send.Text = "2.Send";
+            this.Btn_Send.UseVisualStyleBackColor = true;
+            this.Btn_Send.Click += new System.EventHandler(this.Btn_Send_Click);
+            // 
+            // Btn_Browse
+            // 
+            this.Btn_Browse.Location = new System.Drawing.Point(17, 87);
+            this.Btn_Browse.Margin = new System.Windows.Forms.Padding(15);
+            this.Btn_Browse.Name = "Btn_Browse";
+            this.Btn_Browse.Size = new System.Drawing.Size(82, 29);
+            this.Btn_Browse.TabIndex = 50;
+            this.Btn_Browse.Text = "1.Browse";
+            this.Btn_Browse.UseVisualStyleBackColor = true;
+            this.Btn_Browse.Click += new System.EventHandler(this.Btn_Browse_Click);
+            // 
+            // pb_image
+            // 
+            this.pb_image.Location = new System.Drawing.Point(17, 134);
+            this.pb_image.Name = "pb_image";
+            this.pb_image.Size = new System.Drawing.Size(35, 36);
+            this.pb_image.TabIndex = 51;
+            this.pb_image.TabStop = false;
+            // 
+            // AIdaSendImage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1333, 701);
+            this.Controls.Add(this.pb_image);
+            this.Controls.Add(this.Btn_Browse);
+            this.Controls.Add(this.Btn_Send);
+            this.Controls.Add(this.Tb_Password);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Tb_Account);
+            this.Controls.Add(this.Tb_IP);
+            this.Controls.Add(this.Tb_Port);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label5);
+            this.Font = new System.Drawing.Font("新細明體", 12F);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "AIdaSendImage";
+            this.Text = "AIda Send Image";
+            ((System.ComponentModel.ISupportInitialize)(this.pb_image)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox Tb_Password;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Tb_Account;
+        private System.Windows.Forms.TextBox Tb_IP;
+        private System.Windows.Forms.TextBox Tb_Port;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button Btn_Send;
+        private System.Windows.Forms.Button Btn_Browse;
+        private System.Windows.Forms.PictureBox pb_image;
+    }
+}
+
---
+++ Example C# 8592/AIdaSendImage.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Windows.Forms;
+
+namespace LILIN_Aida_Test
+{
+    public partial class AIdaSendImage : Form
+    {
+        public AIdaSendImage()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Browse 按鈕觸發事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Btn_Browse_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog()
+            {
+                Multiselect = false,
+                Title = "選擇圖片",
+                Filter = "Image files (*.jpg, *.jpeg) | *.jpg; *.jpeg"
+            };
+
+            dialog.ShowDialog();
+            if (dialog.FileName != "")
+            {
+                try
+                {
+                    Image img = Image.FromFile(dialog.FileName);
+
+                    pb_image.Image = img;
+                    pb_image.Width = pb_image.Image.Width;
+                    pb_image.Height = pb_image.Image.Height;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// Send 按鈕觸發事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Btn_Send_Click(object sender, EventArgs e)
+        {
+            string msg = "";
+            byte[] imgbyte = null;
+
+            using (var ms = new MemoryStream())
+            {
+                pb_image.Image.Save(ms, pb_image.Image.RawFormat);
+                imgbyte = ms.ToArray();
+            }
+
+            string remsg = MyHttpPost(@"http://" + Tb_IP.Text + ":" + Tb_Port.Text + "/sendjpeg?channel=17", 5000, imgbyte, Tb_Account.Text, Tb_Password.Text, ref msg);
+
+            if (remsg != "")
+            {
+                Console.WriteLine(remsg);
+            }
+
+            if (msg != "")
+            {
+                Console.WriteLine(msg);
+            }
+        }
+
+        /// <summary>
+        /// 傳送 HTTP POST
+        /// </summary>
+        /// <param name="url"></param>
+        /// <param name="Timeout"></param>
+        /// <param name="content"></param>
+        /// <param name="uaccount"></param>
+        /// <param name="upassword"></param>
+        /// <param name="msg"></param>
+        /// <returns></returns>
+        public static string MyHttpPost(string url, int Timeout, byte[] content, string uaccount, string upassword, ref string msg)
+        {
+            string result = "";
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                request.Method = "POST";
+                request.ContentType = "application/x-www-form-urlencoded";
+                //request.Headers["Content-Description"] = "Test";
+
+                request.Timeout = Timeout;
+
+                if (uaccount != "")
+                {
+                    request.Credentials = new NetworkCredential(uaccount, upassword);
+                }
+
+                #region 添加 Post 参数
+                request.ContentLength = content.Length;
+
+                using (Stream reqStream = request.GetRequestStream())
+                {
+                    reqStream.Write(content, 0, content.Length);
+                    reqStream.Close();
+                }
+                #endregion
+
+                using (HttpWebResponse resp = (HttpWebResponse)request.GetResponse())
+                {
+                    using (Stream stream = resp.GetResponseStream())
+                    {
+                        //獲取響應内容
+                        using (StreamReader reader = new StreamReader(stream, Encoding.UTF8))
+                        {
+                            result = reader.ReadToEnd();
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                result = "";
+                msg = ex.Message;
+            }
+
+            return result;
+        }
+    }
+}
